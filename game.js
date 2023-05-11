76b9531eeb79d00eb755b75f8d5077d9ef4fe767
NiborArSi <Robin-Mommsen@gmx.de>
2023-05-11 17:52:03 +0200

    Macher

--- game.js
+++ game.js
@@ -1,8 +1,25 @@
 const gameboard = document.querySelector("#gameboard")
 
-
+const O_text = "O"
+const X_text = "X"
+let currentPlayer = true;
 
 gameboard.addEventListener("click" ,(e) => {
-    console.log(e.target)
-    e.target.textContent = "X"
+    
+    for(let i = 0; i < 9; i++) {
+    if(currentPlayer) {
+        e.target.textContent = "X"
+    } else (e.target.textContent = "O")
+    currentPlayer = !currentPlayer;
+    }
+    // console.log(e.target)
+    // e.target.textContent = "X"
 })
+
+
+
+
+
+
+
+
---
+++ umrechner.mjs
@@ -0,0 +1,52 @@
+export function umrechner() {
+
+
+const euro = document.querySelector('#eu');
+const kurs = document.querySelector('#ku');
+const dollar = document.querySelector('#do');
+let lastchange = "";
+
+euro.addEventListener("input", () => {
+    const eurovalue = Number(euro.value)
+    const kursvalue = Number(kurs.value)
+    
+    if(isNaN(eurovalue) || isNaN(kursvalue)){
+        alert("Fehler: Mindestens ein Wert entspricht keiner Zahl!")
+    }else{
+        dollar.value = eurovalue * kursvalue;
+        lastchange = "euro";
+    }
+});
+
+dollar.addEventListener("input", () => {
+    const dollarvalue = Number(dollar.value)
+    const kursvalue = Number(kurs.value)
+    
+    if(isNaN(dollarvalue) || isNaN(kursvalue)){
+        alert("Fehler: Mindestens ein Wert entspricht keiner Zahl!")
+    }else{
+        euro.value = dollarvalue / kursvalue;
+        lastchange = "dollar";
+    }
+});
+
+kurs.addEventListener("input", () => {
+    const dollarvalue = Number(dollar.value)
+    const eurovalue = Number(euro.value)
+    const kursvalue = Number(kurs.value)
+    
+    if(isNaN(dollarvalue) || isNaN(eurovalue)){
+        alert("Fehler: Mindestens ein Wert entspricht keiner Zahl!")
+    }else{
+        if(lastchange==="dollar"){
+            euro.value = dollarvalue / kursvalue;
+        } else if (lastchange==="euro"){
+            dollar.value = eurovalue * kursvalue;
+        }
+    }
+});
+
+
+}
+
+
