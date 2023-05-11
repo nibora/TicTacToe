0fc589b87821cba772cddd050e397c7e1ca3506b
NiborArSi <Robin-Mommsen@gmx.de>
2023-05-11 18:57:15 +0200

    Macher

--- game.js
+++ game.js
@@ -1,25 +1,19 @@
 const gameboard = document.querySelector("#gameboard")
-
 const O_text = "O"
 const X_text = "X"
 let currentPlayer = true;
+const feld1 = document.querySelector("#Feld1");
 
 gameboard.addEventListener("click" ,(e) => {
     
-    for(let i = 0; i < 9; i++) {
     if(currentPlayer) {
         e.target.textContent = "X"
     } else (e.target.textContent = "O")
-    currentPlayer = !currentPlayer;
-    }
-    // console.log(e.target)
-    // e.target.textContent = "X"
+    switchPlayer();
+    
+    console.log(feld1.textContent)
 })
 
-
-
-
-
-
-
-
+function switchPlayer() {
+    currentPlayer =!currentPlayer;
+}
